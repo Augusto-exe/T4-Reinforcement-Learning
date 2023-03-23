59d699c5daa9233f0614bf7a6f339bd4dc985458
JuninhoComunista <nicolasparis5@gmail.com>
2023-03-22 22:55:09 -0300

    solve problems 4, 5, 6 and 7

--- src/analysis.py
+++ src/analysis.py
@@ -24,6 +24,7 @@ def question2():
     answerNoise = 0.005
     return answerDiscount, answerNoise
 
+
 def question3a():
     # get the close reward, risking the cliff
     answerDiscount = 0.2
@@ -31,6 +32,7 @@ def question3a():
     answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3b():
     # get the close reward, avoiding the cliff
@@ -40,6 +42,7 @@ def question3b():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3c():
     # Prefer the distant exit (+10), risking the cliff (-10)
     answerDiscount = 0.6
@@ -47,6 +50,7 @@ def question3c():
     answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3d():
     # Prefer the distant exit (+10), avoiding the cliff (-10)
@@ -56,23 +60,25 @@ def question3d():
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3e():
     # Avoid both exits and the cliff (so an episode should never terminate)
-    answerDiscount = 0.01 #1
-    answerNoise = 0.5 #0
-    answerLivingReward = 0 #0.5
+    answerDiscount = 0.01  # 1
+    answerNoise = 0.5  # 0
+    answerLivingReward = 0  # 0.5
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question6():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    return 'NOT POSSIBLE'
     # If not possible, return 'NOT POSSIBLE'
+
 
 if __name__ == '__main__':
     print('Answers to analysis questions:')
     import analysis
+
     for q in [q for q in dir(analysis) if q.startswith('question')]:
         response = getattr(analysis, q)()
         print('  Question %s:\t%s' % (q, str(response)))
--- src/qlearningAgents.py
+++ src/qlearningAgents.py
@@ -16,7 +16,10 @@ from game import *
 from learningAgents import ReinforcementAgent
 from featureExtractors import *
 
-import random,util,math
+import random
+import util
+import math
+
 
 class QLearningAgent(ReinforcementAgent):
     """
@@ -38,11 +41,13 @@ class QLearningAgent(ReinforcementAgent):
         - self.getLegalActions(state)
           which returns legal actions for a state
     """
+
     def __init__(self, **args):
-        "You can initialize Q-values here..."
+        """You can initialize Q-values here..."""
         ReinforcementAgent.__init__(self, **args)
 
         "*** YOUR CODE HERE ***"
+        self.q_values = util.Counter()
 
     def getQValue(self, state, action):
         """
@@ -51,8 +56,7 @@ class QLearningAgent(ReinforcementAgent):
           or the Q node value otherwise
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
+        return self.q_values[(state, action)] if (state, action) in self.q_values else 0.0
 
     def computeValueFromQValues(self, state):
         """
@@ -62,7 +66,8 @@ class QLearningAgent(ReinforcementAgent):
           terminal state, you should return a value of 0.0.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        # if list is empty, returns 0.0
+        return max([self.getQValue(state, action) for action in self.getLegalActions(state)], default=0.0)
 
     def computeActionFromQValues(self, state):
         """
@@ -71,7 +76,12 @@ class QLearningAgent(ReinforcementAgent):
           you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        legal_actions = self.getLegalActions(state)
+        if len(legal_actions) == 0:
+            return None
+
+        max_value = max([self.getQValue(state, action) for action in legal_actions])
+        return random.choice([action for action in legal_actions if self.getQValue(state, action) == max_value])
 
     def getAction(self, state):
         """
@@ -85,12 +95,12 @@ class QLearningAgent(ReinforcementAgent):
           HINT: To pick randomly from a list, use random.choice(list)
         """
         # Pick Action
-        legalActions = self.getLegalActions(state)
-        action = None
+        legal_actions = self.getLegalActions(state)
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
-        return action
+        if len(legal_actions) == 0:
+            return None
+        # if flipCoin than choose randomly else choose best
+        return random.choice(legal_actions) if util.flipCoin(self.epsilon) else self.computeActionFromQValues(state)
 
     def update(self, state, action, nextState, reward):
         """
@@ -101,8 +111,8 @@ class QLearningAgent(ReinforcementAgent):
           NOTE: You should never call this function,
           it will be called on your behalf
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        self.q_values[(state, action)] = (1 - self.alpha) * self.getQValue(state, action) + self.alpha * (
+                reward + self.discount * self.computeValueFromQValues(nextState))
 
     def getPolicy(self, state):
         return self.computeActionFromQValues(state)
@@ -112,9 +122,9 @@ class QLearningAgent(ReinforcementAgent):
 
 
 class PacmanQAgent(QLearningAgent):
-    "Exactly the same as QLearningAgent, but with different default parameters"
+    """Exactly the same as QLearningAgent, but with different default parameters"""
 
-    def __init__(self, epsilon=0.05,gamma=0.8,alpha=0.2, numTraining=0, **args):
+    def __init__(self, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=0, **args):
         """
         These default parameters can be changed from the pacman.py command line.
         For example, to change the exploration rate, try:
@@ -138,8 +148,8 @@ class PacmanQAgent(QLearningAgent):
         informs parent of action for Pacman.  Do not change or remove this
         method.
         """
-        action = QLearningAgent.getAction(self,state)
-        self.doAction(state,action)
+        action = QLearningAgent.getAction(self, state)
+        self.doAction(state, action)
         return action
 
 
@@ -151,6 +161,7 @@ class ApproximateQAgent(PacmanQAgent):
        and update.  All other QLearningAgent functions
        should work as is.
     """
+
     def __init__(self, extractor='IdentityExtractor', **args):
         self.featExtractor = util.lookup(extractor, globals())()
         PacmanQAgent.__init__(self, **args)
@@ -167,6 +178,7 @@ class ApproximateQAgent(PacmanQAgent):
         "*** YOUR CODE HERE ***"
         util.raiseNotDefined()
 
+
     def update(self, state, action, nextState, reward):
         """
            Should update your weights based on transition
@@ -175,7 +187,7 @@ class ApproximateQAgent(PacmanQAgent):
         util.raiseNotDefined()
 
     def final(self, state):
-        "Called at the end of each game."
+        """Called at the end of each game."""
         # call the super-class final method
         PacmanQAgent.final(self, state)
 
